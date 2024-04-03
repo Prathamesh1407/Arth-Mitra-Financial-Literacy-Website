d164332896338261cb225aeea41f77cdea6b71b3
Kushal2311 <kushalmalu2311@gmail.com>
2024-04-03 16:01:49 +0530

    started with creating pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ function App() {
   return (
     <div>
       {/* <Navbar/> */}
-      <Footer/>
+      <Home/>
+      {/* <Footer/> */}
       {/* <LoginPage/> */}
     </div>
   );
---
+++ client/src/Page/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+            <h1>Say 👋🏼 to fininovation </h1>
+            <h1>Your savvy financial companion</h1>
+            <p>Wanna learn how to grow your money to become rich? We have made becoming a stock market Guru easy with our AI powered finance advisor - Arth Sathi 🤵🏻</p>
+            <CustomButton>
+                Chat with ArthaSathi 💬
+            </CustomButton>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
